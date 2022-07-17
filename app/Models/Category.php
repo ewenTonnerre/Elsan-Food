56540bfd4d03f062f0f9c9f39591db8f50946c9e
Ewen Tonnerre <ewen.tonnerre@etudiant.univ-lr.fr>
2022-07-17 23:07:43 +0200

    [FIX] change tables name associated with the models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'category';
 }
